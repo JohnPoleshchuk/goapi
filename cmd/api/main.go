7b5efa74aba2d24a1522f0567b5694e98434e421
JohnPoleshchuk <wellington101823@gmail.com>
2025-10-07 02:13:35 +0300

    add enviornment variables

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,14 +1,21 @@
 package main
 
-import "log"
+import (
+	"log"
+	"os"
+
+	"github.com/JohnPoleshchuk/goapi/internal/env"
+)
 
 func main() {
 	cfg := config{
-		addr: ":8080",
+		addr: env.GetString("ADDR", ":8080"),
 	}
 	app := &application{
 		config: cfg,
 	}
+
+	os.LookupEnv("PATH")
 
 	mux := app.mount()
 	log.Fatal(app.run(mux))
