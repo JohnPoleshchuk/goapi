32a302bf22c2a72ab4fa1a5e7a55c11c91ae3794
JohnPoleshchuk <wellington101823@gmail.com>
2025-10-06 21:48:10 +0300

    transfer from clear net/http to chi

--- cmd/api/api.go
+++ cmd/api/api.go
@@ -4,6 +4,9 @@ import (
 	"log"
 	"net/http"
 	"time"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 )
 
 type application struct {
@@ -14,12 +17,14 @@ type config struct {
 	addr string
 }
 
-func (app *application) mount() *http.ServeMux {
-	mux := http.NewServeMux()
+func (app *application) mount() *chi.Mux {
+	r := chi.NewRouter()
 
-	mux.HandleFunc("GET /v1/health", app.healthCheckHandler)
+	r.Use(middleware.Logger)
 
-	return mux
+	r.Get("health", app.healthCheckHandler)
+
+	return r
 }
 
 func (app *application) run(mux *http.ServeMux) error {
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/JohnPoleshchuk/goapi
 
 go 1.24.7
+
+require github.com/go-chi/chi/v5 v5.2.3 // indirect
